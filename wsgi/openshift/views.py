570bd6d9be6d2bd717164845bae83e516b3fccfe
Yang Wang <y.wang@bom.gov.au>
2014-06-19 13:33:02 +1000

    Test django 1.6.5

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -1,4 +1,5 @@
+import django
 from django.shortcuts import render_to_response
 
 def home(request):
-     return render_to_response('home/home.html')
+    return render_to_response(request, 'home/home.html', {"version": django.VERSION})
