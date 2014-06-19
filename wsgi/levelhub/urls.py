8af0c40415dfb631b251b12a8a4c1e8bd8e644ef
Yang Wang <ywangd@gmail.com>
2014-06-20 00:38:44 +1000

    Test for user and session.

--- wsgi/levelhub/urls.py
+++ wsgi/levelhub/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^register/', 'views.register', name='register'),
+    url(r'^login/', 'views.user_login', name='login'),
+    url(r'^logout/', 'views.user_logout', name='logout'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
--- wsgi/levelhub/views.py
+++ wsgi/levelhub/views.py
@@ -1,5 +1,72 @@
 import django
-from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render_to_response, render
+from django.template import RequestContext
+
+from forms import UserForm
+
 
 def home(request):
-    return render_to_response('home/home.html', {"version": django.VERSION})
+    context = RequestContext(request)
+
+    print 'COOKIES', request.COOKIES
+    print 'context', context
+    print request.session.keys()
+    print request.user
+
+    return render(request, 'home/home.html', {"version": django.VERSION})
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+
+        if user_form.is_valid():
+            user = user_form.save()
+
+            print user.password
+            user.set_password(user.password)
+            print user.password
+            user.save()
+
+            registered = True
+
+        else:
+            print "user_form.errors", user_form.errors
+
+    else:
+        user_form = UserForm()
+
+    return render(request,
+                  'home/register.html',
+                  {'user_form': user_form, 'registered': registered}
+    )
+
+
+def user_login(request):
+    if request.method == 'POST':
+        print "HERE ", request.COOKIES
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            l = login(request, user)
+            print "login: ", l
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse("Invalid login details")
+
+    else:
+        return render(request, 'home/login.html', {})
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/')
